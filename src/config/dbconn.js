61bf89c8aeacaf4dcaf22ebe05613aead0c88e3b
Bhanuka Kamantha <bkamantha@gmail.com>
2023-10-29 19:17:21 +0530

    adding mongodb connnection

---
+++ src/config/dbconn.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const URL = process.env.MONGO_DB_URL;
+
+const connectToMongo = () => {
+  mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
+
+const connection = mongoose.connection;
+
+mongoose.set("strictQuery", false);
+
+connection.on("error", (error) => {
+  console.error(`MongoDB connection error`);
+  setTimeout(connectToMongo, 5000); // Try to reconnect in 5 seconds
+});
+
+connection.once("open", () => {
+  console.log("Mongodb Connection Success !");
+});
+
+
+module.exports.connectToMongo = connectToMongo;
+
