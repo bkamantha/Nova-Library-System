e41b4a75acfdffbb0218e5c8340b56dd1d0d8783
Bhanuka Kamantha <bkamantha@gmail.com>
2023-11-01 18:32:59 +0530

    add token SECRET_KEY

--- src/components/auth/auth.controller.js
+++ src/components/auth/auth.controller.js
@@ -1,16 +1,16 @@
 const jwt = require("jsonwebtoken");
 const { loginUserService } = require("./auth.service");
 const { errorResponse } = require("../../middleware/error-handling-middleware");
+const config = require("../../config");
+const { SECRET_KEY } = config;
 
 const loginUser = async (req, res) => {
   try {
     const { auth, user } = await loginUserService(req.body);
 
-    const token = jwt.sign(
-      { _id: auth?._id, type: user?.type },
-      "YOUR_SECRET_KEY",
-      { expiresIn: "1h" }
-    );
+    const token = jwt.sign({ _id: auth?._id, type: user?.type }, SECRET_KEY, {
+      expiresIn: "1h",
+    });
 
     res.status(200).json({ token, userName: auth._id, userId: user.name });
   } catch (error) {
